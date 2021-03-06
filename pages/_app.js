1e058e1960b8244018b5632904278ce132a2b6b1
Tesfa Demissie <tesfadaniel1@gmail.com>
2021-03-06 02:13:50 -0600

    Header transparent when at top

--- pages/_app.js
+++ pages/_app.js
@@ -12,14 +12,14 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     document.documentElement.lang = 'en';
   });
-
   const [isTop, setTop] = useState(true)
   const [hideOnScroll, setHideOnScroll] = useState(true)
   const [menu, setMenu] = useState(false)
-
+  const [atTop, setAtTop] = useState(true)
   useScrollPosition(({ prevPos, currPos }) => {
     const isShow = currPos.y > prevPos.y
     const atTop = currPos.y > -160;
+    setAtTop(atTop)
     if (isShow !== hideOnScroll && atTop !== isTop) { setHideOnScroll(isShow) }
   }, [hideOnScroll])
 
@@ -29,7 +29,7 @@ function MyApp({ Component, pageProps }) {
     <TypographyStyle />
     <GlobalStyle />
     <DefaultHeadTags />
-    <Header show={hideOnScroll || menu} switch={() => setMenu(!menu)} setMenu={setMenu} />
+    <Header show={hideOnScroll || menu} switch={() => setMenu(!menu)} setMenu={setMenu} transparent={atTop} />
     <Menu menu={menu} switch={() => setMenu(!menu)} />
     <Component {...pageProps} />
     <Footer show={hideOnScroll} />
