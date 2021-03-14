628d1360d85a3b19ef9f4485dc616667d92ec9a3
Tesfa Demissie <tesfadaniel1@gmail.com>
2021-03-13 20:24:36 -0600

    Updated playground projects copy

--- content/PlaygroundProjects.js
+++ content/PlaygroundProjects.js
@@ -5,51 +5,27 @@ const PlaygroundProjects = [
     {
         name: "Modern Color",
         description: `
-        Modern Color is a web application I built to create and 
-        store color schemes for my design work. It uses Vibrant.js 
-        to extract prominent colors from an image. It also uses 
-        Brain.js to check for contrast values for a better user 
-        experience. The color schemes are saved for anyone to access 
-        from anywhere.
+        Modern Color makes it easy to find the perfect 
+        color for your app. Just click on the color that 
+        catches your eye, and you will be able to copy 
+        it into your clipboard in the format you would 
+        like.
         `,
         image: "color",
         tags: "React,SCSS, Firebase",
         link: "https://moderncolor.cc",
         sourceCode: "https://github.com/tesfadan/color"
     },
-    //     {
-    //         name: "TapTempo",
-    //         description: `
-    //         TapTempo is a music production utility tool for finding BPM 
-    //         (beats per minute). With only two keystrokes or mouse clicks, 
-    //         the javascript code calculates the time difference between 
-    //         the input events and displays the current approximate 
-    //         count, improving accuracy upon additional interaction.
-    //         `,
-    //         image: "tempo",
-    //         tags: "HTML,CSS,JS",
-    //         link: "https://taptempo.netlify.app",
-    //         sourceCode: "https://github.com/tesfadan/Tempo"
-    //     },
-    //     {
-    //         name: "Doge Puzzle",
-    //         description: `
-    //         Doge Puzzle is a puzzle game built with vanilla JavaScript. 
-    //         One of the challenges I had to solve for this game was 
-    //         writing an array shuffling algorithm to randomize 
-    //         the puzzle pieces at the start of the game.
-    //         `,
-    //         tags: "JS,CSS,HTML",
-    //         link: "https://tesfadan.github.io/DogePuzzle/",
-    //         sourceCode: "https://github.com/tesfadan/DogePuzzle"
-    //     },
     {
         name: "Portfolio Website",
         description: `
-        I designed and developed my portfolio website with a great 
-        user and developer experience in mind, so I used Next.js 
-        to deliver high-speed performance with Netlify 
-        and GitHub for hosting and continuous delivery.
+        I built this portfolio website to 
+        showcase my skills in design and 
+        development. I applied Next.js's 
+        server-side rendering to deliver a 
+        fast and lean website. I also created 
+        an automated deployment system that 
+        used Github and Netlify.
         `,
         image: "color",
         tags: "Next.js, Netlify",
@@ -57,14 +33,38 @@ const PlaygroundProjects = [
         sourceCode: "https://github.com/tesfadan/portfolio"
     },
     {
+        name: "TapTempo",
+        description: `
+        Tap Tempo creates a seamless experience calculating 
+        the time between beats and displaying the current 
+        approximate beat count, improving accuracy 
+        upon additional interaction.
+        `,
+        image: "tempo",
+        tags: "HTML,CSS,JS",
+        link: "https://taptempo.netlify.app",
+        sourceCode: "https://github.com/tesfadan/Tempo"
+    },
+    {
+        name: "Doge Puzzle",
+        description: `
+
+        Doge Puzzle is a puzzle game built with 
+        vanilla JavaScript, HTML and CSS that 
+        features: Doge, the famous internet meme :)
+        `,
+        tags: "JS,CSS,HTML",
+        link: "https://tesfadan.github.io/DogePuzzle/",
+        sourceCode: "https://github.com/tesfadan/DogePuzzle"
+    },
+    {
         name: "Libby",
         description: `
-        As a way of learning the design software Figma, 
-        I took the initiative to re-design and prototype Libby, 
-        a library app I use for renting audiobooks. Borrowing 
-        design inspirations from streaming apps such as 
-        Spotify and Netflix I was able to design a 
-        clean and modern user interface.
+        Taking inspiration from streaming apps such as Spotify 
+        and Netflix, I designed a clean and modern user 
+        interface for Libby, a library app I use for 
+        borrowing audiobooks. This was a fun weekend 
+        project I used to teach myself Figma.
         `,
         tags: "Figma",
         link: "https://www.figma.com/proto/KmqM8tfRAmQAtKCfoxNwHX/Library-App",
@@ -72,11 +72,10 @@ const PlaygroundProjects = [
     {
         name: "WP Child Theme",
         description: `
-        This was one of the many WordPress projects I worked on 
-        in my second-year Web Development class in college. 
-        The WordPress theme was selected and provided by 
-        the instructor, and my job was to design and 
-        build a child theme.
+        I designed and built a WordPress child theme as 
+        one of my web development projects in college. 
+        My design was for a musician, so it uses plugins 
+        that connect to Youtube and Spotify. 
         `,
         image: "whatuprg",
         tags: "WordPress,PHP,CSS",
