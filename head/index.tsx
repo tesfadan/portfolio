ae10877cb390f28d56e3be774230b26b42417889
Tesfa Demissie <tesfadan@MacBook-Pro.hitronhub.home>
2023-02-01 22:24:39 -0600

    Updated meta tag for portfolio page

--- head/index.tsx
+++ head/index.tsx
@@ -2,6 +2,10 @@ import Head from "next/head";
 import { GA_TRACKING_ID } from "./gtag";
 
 export default function DefaultHeadTags() {
+    const meta = {
+        title: "Tesfa Demissie | Product Designer & Fronend Developer"
+    }
+
 
     return (
         <Head>
@@ -24,7 +28,7 @@ export default function DefaultHeadTags() {
             />
 
             <meta charSet="utf-8" />
-            <title>Tesfa Demissie | Front End Developer, based in Winnipeg, MB.</title>
+            <title>{meta.title}</title>
             <meta name="description" content="I am a Designer and Front End Developer with experience building websites and web applications with JavaScript, HTML, SCSS, and React." />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             {/* Theme and Icons  */}
@@ -35,7 +39,7 @@ export default function DefaultHeadTags() {
             <meta name="theme-color" content="#021B22" />
 
             {/* Twitter metadata  */}
-            <meta name="twitter:title" content="Tesfa Demissie | Front End Developer, based in Winnipeg, MB." />
+            <meta name="twitter:title" content={meta.title} />
             <meta name="twitter:description" content="I am a Designer and Front End Developer with experience building websites and web applications with JavaScript, HTML, SCSS, and React." />
             <meta name="twitter:image" content="/assets/images/tesfadan.com.png" />
             <meta name="twitter:site" content="@tesfadan" />
@@ -43,11 +47,11 @@ export default function DefaultHeadTags() {
 
             {/* Open Graph metadata*/}
             <meta property="og:type" content="website" />
-            <meta property="og:title" content="Tesfa Demissie | Front End Developer, based in Winnipeg, MB." />
+            <meta property="og:title" content={meta.title} />
             <meta property="og:description" content="I am a Designer and Front End Developer with experience building websites and web applications with JavaScript, HTML, SCSS, and React." />
             <meta property="og:image" content="/assets/images/tesfadan.com.png" />
             <meta property="og:url" content="PERMALINK" />
-            <meta property="og:site_name" content="Tesfa Demissie | Front End Developer, based in Winnipeg, MB." />
+            <meta property="og:site_name" content={meta.title} />
             <meta property="og:image" content="/assets/images/tesfadan.com.png" />
             <meta property="og:image:secure_url" content="/assets/images/tesfadan.com.png" />
             <meta property="og:image:type" content="image/png" />
--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import About from '../../components/sections/About';
+import Head from "next/head";
 
 export default function PortfolioPage() {
     const potfolio = {
@@ -10,8 +11,27 @@ export default function PortfolioPage() {
       window.location.replace(potfolio.v1);
     }, []);
 
+
+    const meta = {
+        title: "Tesfa Demissie: Product Designer & Frontend Developer - UI Design & Development Portfolio",
+        content: "As a product designer and frontend developer with expertise in UI design and frontend development, I am passionate about crafting intuitive and engaging user experiences. My portfolio showcases my ability to combine design and technical skills to bring creative solutions to life. I am eager to bring my skills to an innovative company where I can continue to grow and make a meaningful impact.",
+        imagePath: "/assets/images/tesfadan.com.png" 
+    }
+
     return (
         <>
+             <Head>
+                <title>{meta.title}</title>
+                <meta name="description" content={meta.content} />
+                <meta name="twitter:image" content={meta.imagePath} />
+                <meta name="twitter:title" content={meta.title} />
+                <meta name="twitter:description" content={meta.content} />
+                
+                 <meta property="og:site_name" content={meta.title} />
+                <meta property="og:description" content={meta.content} />
+                <meta property="og:image" content={meta.imagePath} />
+                <meta property="og:image:secure_url" content={meta.imagePath} />
+      </Head>
         </>
     );
 }
