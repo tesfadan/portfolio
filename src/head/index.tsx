eb8e6286627aaa9dee41dabeaa8380f32bf5b428
Tesfa Demissie <tesfadan@MacBook-Pro.hitronhub.home>
2023-02-22 20:31:08 -0600

    Updated meta tags content, design focused

--- src/head/index.tsx
+++ src/head/index.tsx
@@ -3,7 +3,8 @@ import { GA_TRACKING_ID } from "./gtag";
 
 export default function DefaultHeadTags() {
     const meta = {
-        title: "Tesfa Demissie | Product Designer & Fronend Developer"
+        title: "Tesfa Demissie | Product Designer based in Winnipeg MB",
+        description: "A passionate Product Designer creating intuitive and visually stunning digital solutions that improve users' lives. I believe in designing products that are both functional and visually appealing, always keeping the user's needs at the forefront of my mind."
     }
 
 
@@ -29,7 +30,7 @@ export default function DefaultHeadTags() {
 
             <meta charSet="utf-8" />
             <title>{meta.title}</title>
-            <meta name="description" content="I am a Designer and Front End Developer with experience building websites and web applications with JavaScript, HTML, SCSS, and React." />
+            <meta name="description" content={meta.description} />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             {/* Theme and Icons  */}
             <meta name="theme-color" content="#F6F3F0" />
@@ -39,7 +40,7 @@ export default function DefaultHeadTags() {
 
             {/* Twitter metadata  */}
             <meta name="twitter:title" content={meta.title} />
-            <meta name="twitter:description" content="I am a Designer and Front End Developer with experience building websites and web applications with JavaScript, HTML, SCSS, and React." />
+            <meta name="twitter:description" content={meta.description} />
             <meta name="twitter:image" content="/assets/images/tesfadan.com.png" />
             <meta name="twitter:site" content="@tesfadan" />
             <meta name="twitter:creator" content="@tesfadan" />
@@ -47,19 +48,18 @@ export default function DefaultHeadTags() {
             {/* Open Graph metadata*/}
             <meta property="og:type" content="website" />
             <meta property="og:title" content={meta.title} />
-            <meta property="og:description" content="I am a Designer and Front End Developer with experience building websites and web applications with JavaScript, HTML, SCSS, and React." />
+            <meta property="og:description" content={meta.description} />
             <meta property="og:image" content="/assets/images/tesfadan.com.png" />
             <meta property="og:url" content="PERMALINK" />
             <meta property="og:site_name" content={meta.title} />
             <meta property="og:image" content="/assets/images/tesfadan.com.png" />
             <meta property="og:image:secure_url" content="/assets/images/tesfadan.com.png" />
             <meta property="og:image:type" content="image/png" />
-            <meta property="og:image:width" content="1920" />
-            <meta property="og:image:height" content="1080" />
-            <meta property="og:image:alt" content="Tesfa Demissie | Front End Developer, based in Winnipeg, MB." />
+            <meta property="og:image:width" content="1512" />
+            <meta property="og:image:height" content="977" />
+            <meta property="og:image:alt" content={meta.title} />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600&display=swap" rel="stylesheet" />
-
+            {/* <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600&display=swap" rel="stylesheet" /> */}
         </Head>
     )
 }
