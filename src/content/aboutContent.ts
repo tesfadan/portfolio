4e7a703a38d9faf6518b101574b224fee10b3a70
Tesfa Demissie <tesfadan@MacBook-Pro.hitronhub.home>
2023-02-27 12:02:09 -0600

    Updated resume

--- src/content/aboutContent.ts
+++ src/content/aboutContent.ts
@@ -1,22 +1,22 @@
 export default [
-        {
-            section: "Summary",
-            content: "I'm Tesfa Demissie, a product designer with a solid foundation in design and a strong understanding of development. My design philosophy centres on the end user, and I strive to build user interfaces that solve their problems first and foremost with intuitive, aesthetically pleasing and user-friendly designs."
-        },
-        {
-            section: "Skillset",
-            content: "I'm a multi-faceted designer with skills in UI design, prototyping, front-end development & visual design. And well-versed in design tools such as Figma, Adobe XD, Photoshop, Illustrator & After Effects. And for front-end development, I have experience working with JavaScript, HTML, CSS, React and React Native."
-        },
-        {
-            section: "Experience",
-            content: "My +5 years of work experience with digital media across mediums and industries shapes how I perceive design problems and provides me with a unique insight to find fitting solutions. Currently, I work at Combine Venture Builders as a product designer, where I spend my days providing design services at every stage of a startup lifecycle. My contribution at Combine ranges from sketching wireframes and user flows, designing dev-ready user interfaces, and building functional & interactive prototypes. See my portfolio for more."
-        },
-        {
-            section: "Interests",
-            content: "In my free time, I enjoy listening to music, reading books on philosophy and having meaningful conversations with my loved ones."
-        },
-        {
-            section: "Contact",
-            content: "If you'd like to get in touch, reach out at [tesfa@tesfadan.com](mailto:tesfa@tesfadan.com). I look forward to hearing from you."
-        }
+    {
+        section: "Summary",
+        content: "I'm Tesfa Demissie, a product designer with a solid foundation in design and a strong understanding of development. My design philosophy centres on the end user, and I strive to build user interfaces that solve their problems first and foremost with intuitive, aesthetically pleasing and user-friendly designs."
+    },
+    {
+        section: "Skillset",
+        content: "I'm a multi-faceted designer with skills in UI design, prototyping, front-end development & visual design. And well-versed in design tools such as Figma, Adobe XD, Photoshop, Illustrator & After Effects. For front-end development, I have experience working with JavaScript, HTML, CSS, React and React Native."
+    },
+    {
+        section: "Experience",
+        content: "My +5 years of work experience with digital media across mediums and industries shapes how I perceive design problems and provides me with a unique insight to find fitting solutions. Currently, I work at Combine Venture Builders as a product designer, where I spend my days providing design services at every stage of a startup lifecycle. My contribution at Combine ranges from sketching wireframes and user flows, designing dev-ready user interfaces, and building functional & interactive prototypes. See my portfolio for more."
+    },
+    {
+        section: "Interests",
+        content: "In my free time, I enjoy listening to music, reading books on philosophy and having meaningful conversations with my loved ones."
+    },
+    {
+        section: "Contact",
+        content: "If you'd like to get in touch, reach out at [tesfa@tesfadan.com](mailto:tesfa@tesfadan.com). I look forward to hearing from you."
+    }
 ]
